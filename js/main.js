86401a61f7748de9eab932a10aa4d5c710886c7c
David Weid <david.weid.2@gmail.com>
2019-03-05 17:34:57 -0500

    Update to test2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-$(document).ready(function () {
-    $(".sidenav").sidenav();
-    $(".slider").slider();
-    $(".scrollspy").scrollSpy();
-    $(".fixed-action-btn").floatingActionButton();
+// $(document).ready(function () {
+//     $(".sidenav").sidenav();
+//     $(".slider").slider();
+//     $(".scrollspy").scrollSpy();
+//     $(".fixed-action-btn").floatingActionButton();
+// })
+
+$(document).ready(function() {
+    $("#works-btn").on("click", function() {
+
+        if ($(this).data("status") === "off") {
+            $(this).data("status", "on");
+            $(".page-two").css("height", "100%");
+            $(this).text("Home");
+
+        } else if ($(this).data("status") === "on") {
+            $(this).data("status", "off");
+            $(".page-two").css("height", "0%");
+            $(this).text("Works");
+        }
+
+    });
+
+    $("#contact-btn").on("click", function() {
+
+        if ($(this).data("status") === "off") {
+            $(this).data("status", "on");
+            $(".page-three").css("height", "100%");
+            $(this).text("Home");
+
+        } else if ($(this).data("status") === "on") {
+            $(this).data("status", "off");
+            $(".page-three").css("height", "0%");
+            $(this).text("Contact");
+        }
+
+    });
 })
