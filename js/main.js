61caee530604ab432db5fadeae22f04bf1ffd073
David Weid <david.weid.2@gmail.com>
2019-04-09 15:12:37 -0400

    changed js for pages up/down

--- js/main.js
+++ js/main.js
@@ -1,94 +1,98 @@
 $(document).ready(function() {
+  console.log("Ready");
 
-    $("#works-btn").on("click", function() {
-        if($(this).data("status") === "off") {
-            worksPageUp();
-            contactPageDown();
-            pageOneDown();
-        } else {
-            worksPageDown();
-            contactPageDown();
-            pageOneUp();
-        }
-    });
-
-    $("#contact-btn").on("click", function() {
-        if($(this).data("status") === "off") {
-            contactPageUp();
-            worksPageDown();
-            pageOneDown();
-        } else {
-            contactPageDown();
-            worksPageDown();
-            pageOneUp();
-        }
-    });
-
-    $("#email-btn").on("click", function() {
-        if($(this).data("status") === "off") {
-            $(this).data("status", "on");
-            contactBtnDivUp();
-            contactFormUp();
-        } else {
-            $(this).data("status", "off");
-            contactBtnDivDown();
-            contactFormDown();
-        }
-    });
-
-    var contactBtnDivUp = function() {
-        $(".contactBtn-row").css("height", "25%");
-        $("#contactBtnDiv").css("top", "18%");    
-    };
-
-    var contactBtnDivDown = function() {
-        $(".contactBtn-row").css("height", "100%");
-        $("#contactBtnDiv").css("top", "50%");
-    };
-
-    var contactFormUp = function() {
-        $(".contactForm-row").css("height", "75%");   
-    };
-
-    var contactFormDown = function() {
-        $(".contactForm-row").css("height", "0%");
+  $("#works-btn").on("click", function() {
+    if ($(this).data("status") === "off") {
+      worksPageUp();
+    //   contactPageDown();
+    //   pageOneDown();
+    } else {
+    //   worksPageDown();
+    //   contactPageDown();
+      pageOneUp();
     }
+  });
 
-    var pageOneUp = function() {
-        $(".page-one").css("opacity", "1");
-        $(".full-container").css("background", "rgba(0,0,0,0.3)");
-    };
-
-    var pageOneDown = function() {
-        $(".page-one").css("opacity", "0");
-        $(".full-container").css("background", "rgba(0,0,0,1)");
+  $("#contact-btn").on("click", function() {
+    if ($(this).data("status") === "off") {
+      contactPageUp();
+    //   worksPageDown();
+    //   pageOneDown();
+    } else {
+    //   contactPageDown();
+    //   worksPageDown();
+      pageOneUp();
     }
+  });
 
-    var worksPageUp = function() {
-        $("#works-btn").data("status", "on");
-        $(".page-two").css("height", "100%");
-        $("#works-btn").text("HOME");
-    };
+  $("#email-btn").on("click", function() {
+    if ($(this).data("status") === "off") {
+      $(this).data("status", "on");
+      contactBtnDivUp();
+      contactFormUp();
+    } else {
+      $(this).data("status", "off");
+      contactBtnDivDown();
+      contactFormDown();
+    }
+  });
 
-    var worksPageDown = function() {
-        $("#works-btn").data("status", "off");
-        $(".page-two").css("height", "0%");
-        $("#works-btn").text("WORKS");
-    };
+  var contactBtnDivUp = function() {
+    $(".contactBtn-row").css("height", "25%");
+    $("#contactBtnDiv").css("top", "18%");
+  };
 
-    var contactPageUp = function() {
-        
-        $("#contact-btn").data("status", "on");
-        $(".page-three").css("height", "100%");
-        $("#contact-btn").text("HOME");
-    };
-    
-    var contactPageDown = function() {
-    
-        $("#contact-btn").data("status", "off");
-        $(".page-three").css("height", "0%");
-        $("#contact-btn").text("CONTACT");
-    };
+  var contactBtnDivDown = function() {
+    $(".contactBtn-row").css("height", "100%");
+    $("#contactBtnDiv").css("top", "50%");
+  };
 
-})
+  var contactFormUp = function() {
+    $(".contactForm-row").css("height", "75%");
+  };
+
+  var contactFormDown = function() {
+    $(".contactForm-row").css("height", "0%");
+  };
+
+  var pageOneUp = function() {
+    $(".page-one").css("opacity", "1");
+    $(".full-container").css("background", "rgba(0,0,0,0.3)");
+    worksPageDown();
+    contactPageDown();
+  };
+
+  var pageOneDown = function() {
+    $(".page-one").css("opacity", "0");
+    $(".full-container").css("background", "rgba(0,0,0,1)");
+  };
+
+  var worksPageUp = function() {
+    $("#works-btn").data("status", "on");
+    $(".page-two").css("height", "100%");
+    $("#works-btn").text("HOME");
+    pageOneDown();
+    contactPageDown();
+  };
+
+  var worksPageDown = function() {
+    $("#works-btn").data("status", "off");
+    $(".page-two").css("height", "0%");
+    $("#works-btn").text("WORKS");
+  };
+
+  var contactPageUp = function() {
+    $("#contact-btn").data("status", "on");
+    $(".page-three").css("height", "100%");
+    $("#contact-btn").text("HOME");
+    pageOneDown();
+    worksPageDown();
+  };
+
+  var contactPageDown = function() {
+    $("#contact-btn").data("status", "off");
+    $(".page-three").css("height", "0%");
+    $("#contact-btn").text("CONTACT");
+  };
+});
 // Document.ready
