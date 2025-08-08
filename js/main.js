3d518b8228414d3b76d454750f0a1649eb9d9e98
David J Weid II <david.weid.2@gmail.com>
2025-08-07 21:11:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  console.log("Ready");
-
   // lazy loads elements with default selector as '.lozad'
   const observer = lozad();
   observer.observe();
@@ -148,5 +146,7 @@ $(document).ready(function() {
     $(".page-three").css("height", "0%");
     $("#contact-btn").text("CONTACT");
   };
+
+  pageOneUp();
 });
 // Document.ready
